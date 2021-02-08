c1740f15f88bc15bfc9f0731d4289446218fb013
nath <nathrijacobs@gmail.com>
2021-02-08 12:51:43 +0200

    renamed package

---
+++ org-data-analytics-webapp/src/main/java/org/data/analytics/DataAnalyticsWebApp.java
@@ -0,0 +1,14 @@
+package org.data.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalyticsWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAnalyticsWebApp.class, args);
+	}
+
+
+}
